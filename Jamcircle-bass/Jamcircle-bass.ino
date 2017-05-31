e96f60a85d113d1c4bfcebcc6d2d0a280ffcf929
Arvid @ soundlab PC <arvid@arvidjense.com>
2017-05-31 18:31:55 +0200

    Added encoder

--- Jamcircle-bass/Jamcircle-bass.ino
+++ Jamcircle-bass/Jamcircle-bass.ino
@@ -1,15 +1,17 @@
 //teensy 3.0 with pwm output
 
 #include <Audio.h>
-
+#include <Encoder.h>
 // GUItool: begin automatically generated code
 AudioSynthWaveform       waveform1;      //xy=521,377
 AudioOutputPWM           pwm1;           //xy=726,390
 AudioConnection          patchCord1(waveform1, pwm1);
 // GUItool: end automatically generated code
-
+Encoder myEnc(14, 15);
+long oldPosition  = -999;
 
 void setup() {
+  Serial.begin(9600);
   AudioMemory(2);
   waveform1.begin(WAVEFORM_SQUARE);
   waveform1.frequency(20);
@@ -17,5 +19,10 @@ void setup() {
 }
 
 void loop() {
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
   delay(10);
 }
