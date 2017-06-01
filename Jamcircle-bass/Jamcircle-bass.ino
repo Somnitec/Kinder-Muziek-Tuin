cfb7ad1474a50ce8a04507e3b4f52720d9a5f98a
Arvid @ soundlab PC <arvid@arvidjense.com>
2017-06-01 14:56:07 +0200

    All sensors working

--- Jamcircle-bass/Jamcircle-bass.ino
+++ Jamcircle-bass/Jamcircle-bass.ino
@@ -1,5 +1,10 @@
 //teensy 3.0 with pwm output
 
+#define stickXpin A5
+#define stickYpin A4
+#define wheelpin1 14
+#define wheelpin2 15
+
 #include <Audio.h>
 #include <Encoder.h>
 // GUItool: begin automatically generated code
@@ -7,8 +12,10 @@ AudioSynthWaveform       waveform1;      //xy=521,377
 AudioOutputPWM           pwm1;           //xy=726,390
 AudioConnection          patchCord1(waveform1, pwm1);
 // GUItool: end automatically generated code
-Encoder myEnc(14, 15);
+Encoder myEnc(wheelpin1, wheelpin2);
 long oldPosition  = -999;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -16,6 +23,8 @@ void setup() {
   waveform1.begin(WAVEFORM_SQUARE);
   waveform1.frequency(20);
   waveform1.amplitude(0.01);
+  pinMode(stickXpin, INPUT);
+  pinMode(stickYpin, INPUT);
 }
 
 void loop() {
@@ -24,5 +33,10 @@ void loop() {
     oldPosition = newPosition;
     Serial.println(newPosition);
   }
+  //Serial.printf(analogRead(stickXpin),analogRead(stickYpin));
+
+  Serial.print(analogRead(stickXpin));
+  Serial.print("\t");
+  Serial.println(analogRead(stickYpin));
   delay(10);
 }
